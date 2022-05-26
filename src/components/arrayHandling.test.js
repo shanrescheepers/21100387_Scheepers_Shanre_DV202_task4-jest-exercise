7139059e38144f61b8671e3641e0d9803aabff57
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-05-26 02:05:26 +0200

    TEST 4 PASSED
    Two ways of passing test 4 -> need second opinion

--- src/components/arrayHandling.test.js
+++ src/components/arrayHandling.test.js
@@ -40,4 +40,38 @@ describe("Test Suite for Handling an Array..", () => {
         //This passes too?
     })//END OF TEST 2
 
+    //TEST 3)
+    //adds a new item to the end of an array
+    //export const pushItemToEndOfArray = (array, item) => {
+    //   var newArray = array
+    //   newArray.push(item)
+    //   return newArray}
+    test("If an item is added to the end of the array", () => {
+        const anArray = [1, 2, 3, 4, 5, 1, 2];
+        const newArrayItem = 2;
+
+        let newArray = pushItemToEndOfArray(anArray, newArrayItem);
+        expect(newArray).toBe(anArray, newArrayItem)
+        // const anArray: number[] is what the above says. This Passes?
+    }) //END OF TEST 3
+
+    //Test 4
+    //adds a new item to the start of an array
+    //export const pushItemToStartOfArray = (array, item) => {
+    //  var newArray = array
+    //newArray.unshift(item)
+    //return newArray
+    test("If an item is added to the start of the array", () => {
+        const anArray = [1, 2, 3, 4, 5, 1, 2];
+        const newArrayItem = 2;
+
+        let newArray = pushItemToStartOfArray(anArray, newArrayItem);
+        expect(newArray[0]).toBe((newArrayItem));
+        // This finally passed
+        expect(newArray).toEqual([2, 1, 2, 3, 4, 5, 1, 2]);
+        // Dpuble check, this also passed, so there's a new array with 2 at the start
+    }) //END OF TEST 4
+
+    //TEST 5
+
 })
